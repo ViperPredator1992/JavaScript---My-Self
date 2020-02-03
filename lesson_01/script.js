8e797f351e37e5a6479349d533aebdd18598ede0
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2020-02-03 22:08:24 +0200

    add lesson01

---
+++ lesson_01/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let money, // деньги
+    income, // доход
+    addExpenses, // добавить расходы
+    deposit, // депозит
+    mission, // миссия
+    period; // период
+
+alert( 'Привет, JS' );
+console.log( 'Привет, JS' );
